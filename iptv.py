1917798c2c322b230064cc57c9ebc504e5accee0
homefelix08-dotcom <homefelix08@gmail.com>
2026-05-01 10:47:40 -0300

    Add files via upload

--- iptv.py
+++ iptv.py
@@ -7,7 +7,8 @@ from urllib3.util.retry import Retry
 
 # Cabeçalhos para fingir que a requisição vem de um navegador real
 HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "User-Agent": "Mozilla/5.0 ...",
+    "X-Forwarded-For": "177.129.1.1",
     "Referer": "https://4embeddecanais.xyz/"
 }
 
